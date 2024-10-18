149a61f88c043ccd0b12a2634f7e60d3c63b2d84
flaviocwz <flavio.cruz@dcomp.ufs.br>
2024-10-18 10:47:41 -0300

    adicionando lista de palavras.js

---
+++ main.js
@@ -0,0 +1,4 @@
+// Criando os arrays que irão conter as palavras possíveis a serem formadas
+const palavras1 = ["aborto", "talho", "torta", "lata", "rolar", "toalha", "trabalho", "barato", "tralha", "botao"]
+const palavras2 = ["marte", "metro", "mestre", "astro", "estar", "tomar", "resto", "aroma", "terra", "rastro"]
+const palavras3 = ["vaso", "selo", "salto", "esta", "estalo", "veste","sol", "seta", "salvo","selva"]
