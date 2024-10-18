b7ac5272218281e74c84f7b81c1663ed39e73fc3
flaviocwz <flavio.cruz@dcomp.ufs.br>
2024-10-18 10:37:42 -0300

    dicionando lista de palavras

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // JavaScript - Salvo em um arquivo separado chamado 'main.js'
 // Função para verificar se a palavra formada está na lista e contém a letra obrigatória
 const verificarPalavra = (palavra, listaPalavras, letraObrigatoria) => 
@@ -104,3 +105,9 @@ const renderizarPalavraFormada = (palavra) => {
 document.addEventListener('DOMContentLoaded', () => {
     iniciarJogo();
 });
+=======
+// Criando os arrays que irão conter as palavras possíveis a serem formadas
+const palavras1 = ["aborto", "talho", "torta", "lata", "rolar", "toalha", "trabalho", "barato", "tralha", "botao"]
+const palavras2 = ["marte", "metro", "mestre", "astro", "estar", "tomar", "resto", "aroma", "terra", "rastro"]
+const palavras3 = ["vaso", "selo", "salto", "esta", "estalo", "veste","sol", "seta", "salvo","selva"]
+>>>>>>> 48791d2 (adicionando lista de palavras)
