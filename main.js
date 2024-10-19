6ca75465627266b69dadacce721d84c8fb59be35
Mizael Paris <mizael.leite@dcomp.ufs.br>
2024-10-19 10:30:21 -0300

    Modificando o estilo da página

---
+++ main.js
@@ -0,0 +1,3 @@
+// Criando os arrays que irão conter as palavras possíveis a serem formadas
+const palavras1 = ["aborto", "talho", "torta", "lata", "rolar", "toalha", "trabalho", "barato", "tralha", "botao"]
+const palavras2 = ["marte", "metro", "mestre", "astro", "estar", "tomar", "resto", "aroma", "terra", "rastro"]
