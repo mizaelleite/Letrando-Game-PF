4d3a48994305a2706a8167b02958ddf1e8b4ac66
Mizael Paris <mizael.leite@dcomp.ufs.br>
2024-10-19 10:37:23 -0300

    Modificando o estilo da página

---
+++ main.js
@@ -0,0 +1,3 @@
+const palavras1 = ["aborto", "talho", "torta", "lata", "rolar", "toalha", "trabalho", "barato", "tralha", "botao"]
+const palavras2 = ["marte", "metro", "mestre", "astro", "estar", "tomar", "resto", "aroma", "terra", "rastro"]
+const palavras3 = ["vaso", "selo", "salto", "esta", "estalo", "veste","sol", "seta", "salvo","selva"] 
